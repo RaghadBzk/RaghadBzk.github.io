54c8d86d307c3614a16fc255f6b5594d3e74c8ce
raghad baizouk <raghad.baizouk@expentech.com>
2023-04-30 22:55:44 +0300

    init commit

---
+++ src/Data.jsx
@@ -0,0 +1,433 @@
+import Project1 from "./assets/project1.png";
+import Project2 from "./assets/project2.png";
+import Project3 from "./assets/project3.png";
+import Project4 from "./assets/project4.png";
+import Project5 from "./assets/project5.png";
+import Project6 from "./assets/project6.png";
+import Project7 from "./assets/project7.png";
+import Project8 from "./assets/project8.png";
+import Project9 from "./assets/project9.png";
+
+// import Testimonial1 from './assets/testimonial1.jpg';
+// import Testimonial2 from './assets/testimonial2.jpg';
+// import Testimonial3 from './assets/testimonial3.jpg';
+// import Testimonial4 from './assets/testimonial4.jpg';
+// import Testimonial5 from './assets/testimonial5.jpg';
+
+export const links = [
+  {
+    name: "Home",
+    path: "home",
+  },
+  // {
+  //   name: "Services",
+  //   path: "services",
+  // },
+  {
+    name: "Skills",
+    path: "skills",
+  },
+  {
+    name: "Works",
+    path: "work",
+  },
+  {
+    name: "Resume",
+    path: "resume",
+  },
+  // {
+  //   name: 'Testimonials',
+  //   path: 'testimonial',
+  // },
+  // {
+  //   name: 'Pricing',
+  //   path: 'pricing',
+  // },
+  // {
+  //   name: 'Blog',
+  //   path: 'blog',
+  // },
+  {
+    name: "Contact",
+    path: "contact",
+  },
+];
+
+export const services = [
+  {
+    id: 1,
+    name: "front Development",
+    title: "I have +2 years Angular experience ",
+    description:
+      "Transforming the design into interfaces, starting from scratch. ",
+  },
+  {
+    id: 2,
+    name: "front Development",
+    title: "I have +1 year React experience ",
+    description:
+      "Transforming the design into interfaces, starting from scratch and deploy projects on the server ",
+
+      
+  },
+  {
+    id: 3,
+    name: "front Development",
+    title: "I have six months Vue experience ",
+    description:
+      "Transforming the design into interfaces, starting from scratch and improve SEO ",
+  },
+  {
+    id: 4,
+    name: "analysis",
+    title: "I have +1 year Database experience && Oracle procedures  ",
+    description:
+      "The possibility of analyzing the system and collecting work requirements from the customer accurately and Writing procedures in Oracle and dealing with it ERP systems.",
+  },
+  {
+    id: 5,
+    name: " Back Development ",
+    title: " I have +1 year asp.net experience ",
+    description:
+      "create Rest API",
+  },
+ 
+
+];
+
+export const skills = [
+  {
+    id: 1,
+    name: "React",
+    percentage: 85,
+    description:
+      "",
+  },
+
+  {
+    id: 2,
+    name: "Angular ",
+    percentage: 75,
+    description:
+      "",
+  },
+
+  {
+    id: 3,
+    name: "JavaScript",
+    percentage: 90,
+    description:
+      "",
+  },
+
+  {
+    id: 4,
+    name: "TypeScript",
+    percentage: 75,
+    description:
+      "",
+  },
+
+  {
+    id: 5,
+    name: "Bootstrap",
+    percentage: 85,
+    description:
+      "",
+  },
+
+  {
+    id: 6,
+    name: "Tailwind css",
+    percentage: 65,
+    description:
+      "",
+  },
+  {
+    id: 7,
+    name: "Material UI",
+    percentage: 65,
+    description:
+      "",
+  },
+  {
+    id: 8,
+    name: "Redux, Redux Toolkit",
+    percentage: 65,
+    description:
+      "",
+  },
+  {
+    id: 9,
+    name: "ReactNative",
+    percentage: 50,
+    description:
+      "",
+  },
+  {
+    id: 10,
+    name: "Oracle, My SQL, SQL",
+    percentage: 65,
+    description:
+      "",
+  },
+  {
+    id: 11,
+    name: "WordPress",
+    percentage: 65,
+    description:
+      "",
+  },
+  {
+    id: 12,
+    name: "Git",
+    percentage: 90,
+    description:
+      "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    img: Project1,
+    category: "React",
+    title: "Online Ticketing WebSite",
+    path: "https://onlineticket.expentech.com",
+    description:
+      "ِAproject managing online reservations for transport companies in Syria.",
+    description1: "Number of Font Team: 2",
+  },
+  {
+    id: 2,
+    img: Project2,
+    category: "React",
+    title: "Online Ticketing Dashboard",
+    path: "http://ot.expentech.com",
+    description:
+      "Aproject managing online reservations for transport companies in Syria.",
+    description1: "Number of Font Team: 2",
+  },
+  {
+    id: 3,
+    img: Project3,
+    category: "Angular/Oracle/Asp.net",
+    title: "Mahrocat Company ERP",
+    path: "https://dev03.tatweer.sy/3isadcop/",
+    description:
+      "A project to manage Syria fuels with username:Admin and password:123",
+    description1: "Number of Font Team: 3",
+  },
+ 
+  {
+    id: 4,
+    img: Project4,
+    category: "js/Bootstrap",
+    title: "BOA",
+    path: "https://eloquent-fairy-5d0f8b.netlify.app",
+    description:
+      "A project to study the psychological state of employees at Bank Aljazira.",
+    description1: "Number of Font Team: 2",
+  },
+  {
+    id: 5,
+    img: Project5,
+    category: "Angular",
+    title: "Distinction and Creativity Agency",
+    path: "https://dev02.tatweer.sy/dcaf/",
+    description:
+      "A project to issue grades for middle school students with username:admin and password:tatweer.",
+      description1:
+      'Number of Font Team: 5',
+  },
+  {
+    id: 6,
+    img: Project6,
+    category: "React",
+    title: "IJK",
+    path: "https://ijk-front.expentech.com",
+    description:
+      "A Project for booking Trips in UAE with username:admin and password:admin123 .",
+      description1:
+      'Number of Font Team: 2',
+  },
+  {
+    id: 7,
+    img: Project7,
+    category: "React",
+    title: "Lertna",
+    path: "https://lertnafront.expentech.com",
+    description:
+      "A project to issue grades for middle school students with username:0965111076 and password:password.",
+      description1:
+      'Number of Font Team: 2',
+  },
+
+  {
+    id: 9,
+    img: Project9,
+    category: "JavaScript",
+    title: "Robot",
+    path: "https://robot.expentech.com/",
+    description: "A project to connect with a robot to detect rubble",
+    description1:
+    'Number of Font Team: only me',
+  },
+];
+
+export const cv = [
+  {
+    id: 2,
+    title: "Master in Software and Information Systems Engineering",
+    subtitle: "Damascus University, syria",
+    date: "SEP 2020 - PRESENT",
+    description:
+      "I finished all my courses and I am working on my masters thesis.",
+    category: "education",
+  },
+
+  {
+    id: 3,
+    title: "Bachelor in Software Engineering",
+    subtitle: "AL-Baath University, syria",
+    date: "SEP 2014 - AUG 2019",
+    description: "Top 15 in my class.",
+    category: "education",
+  },
+
+  {
+    id: 4,
+    title: "FrontEnd Developer",
+    subtitle: "EXPentech solution, syria",
+    date: "July 2022 - PRESENT",
+    description:
+      "Designed UI & integrated code for +4 front-end projects using React.",
+
+    description1:
+      "Designed UI for +2 projects using pure HTML5,CSS3,Bootstrap.",
+
+    description2: "Write clean code, fix bugs and solve problems.",
+    description3: "Collaborate with design team to create user interfaces.",
+    description4: "Deploy projects on the server.",
+
+    category: "experience",
+  },
+
+  {
+    id: 5,
+    title: "Teaching Assistant",
+    subtitle: "IT university, syria",
+    date: "SEP 2021 - MAR 2023",
+    description: "Teaching c++ language.",
+    description1: "Teaching java language.",
+    description2: "Supervisor for +3 IT projects.",
+    category: "experience",
+  },
+
+  {
+    id: 6,
+    title: " Software Developer",
+    subtitle: "Tatweer, syria",
+    date: "July 2020 - July 2022",
+    description:
+      "Designed UI & integrated code for +3 front-end projects using Angular.",
+    description1:
+      "Developed and maintained code for +1 back-end project using Asp.net and Oracle.",
+    description2:
+      "Communicate with customer, collect and discuss requirements.",
+    category: "experience",
+  },
+];
+
+export const photos = [
+  {
+    title:"project1",
+    src: Project2,
+    width: 4,
+    height: 3
+  },
+  {
+    src: Project2,
+    width: 1,
+    height: 1
+  },
+  {
+    src: Project2,
+    width: 3,
+    height: 4
+  },
+  {
+    src: Project2,
+    width: 3,
+    height: 4
+  },
+  {
+    src: Project2,
+    width: 3,
+    height: 4
+  },
+  {
+    src: Project2,
+    width: 4,
+    height: 3
+  },
+  {
+    src: Project2,
+    width: 3,
+    height: 4
+  },
+  {
+    src: Project2,
+    width: 4,
+    height: 3
+  },
+  {
+    src: Project2,
+    width: 4,
+    height: 3
+  }
+];
+
+
+// export const testimonials = [
+//   {
+//     id: 1,
+//     img: Testimonial1,
+//     name: 'Barbara Wilson',
+//     author: 'CEO Company',
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+//   },
+//   {
+//     id: 2,
+//     img: Testimonial2,
+//     name: 'Charlie Smith',
+//     author: 'Designer',
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+//   },
+//   {
+//     id: 3,
+//     img: Testimonial3,
+//     name: 'Roy Wang',
+//     author: 'Manager GYM',
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+//   },
+//   {
+//     id: 4,
+//     img: Testimonial4,
+//     name: 'Jennifer Smith',
+//     author: 'CEO & Founder',
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+//   },
+//   {
+//     id: 5,
+//     img: Testimonial5,
+//     name: 'Paul Freeman',
+//     author: 'Photographer',
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+//   },
+// ];
